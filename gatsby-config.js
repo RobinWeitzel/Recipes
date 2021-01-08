8073dcf2f51d4c9b1d212053c521b29e739beee2
Robin Weitzel <robin.weitzel.rw@gmail.com>
2021-01-08 15:16:54 +0100

    switched to Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "My cookbook",
+  },
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
